daac99a980c2f3fd75aa9070b685166cef94a0d5
Ryan Penfold <rpenfold@live.com>
2021-05-29 08:10:13 -0600

    updated dependencies

--- src/__tests__/index.unit.test.ts
+++ src/__tests__/index.unit.test.ts
@@ -1,11 +1,21 @@
-import PromisePortal, { ComponentRegistry } from "..";
+import PromisePortal, { ComponentRegistry, usePromisePortal, withPromisePortal } from "..";
 
 describe("index", () => {
   it("PromisePortal export works correctly", () => {
     expect(PromisePortal).toBeDefined();
+    expect(PromisePortal.Provider).toBeDefined();
+    expect(PromisePortal.showPortalAsync).toBeDefined();
   });
 
   it("ComponentRegistry export works correctly", () => {
     expect(ComponentRegistry).toBeDefined();
+  });
+
+  it("withPromisePortal export works correctly", () => {
+    expect(withPromisePortal).toBeDefined();
+  });
+
+  it("usePromisePortal export works correclty", () => {
+    expect(usePromisePortal).toBeDefined();
   });
 });
