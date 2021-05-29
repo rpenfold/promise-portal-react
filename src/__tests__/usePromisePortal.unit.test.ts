7506c2c4183cb8a0840c83e7ed7728c5ac742139
Ryan Penfold <rpenfold@live.com>
2021-05-28 22:43:12 -0600

    tests for usePromisePortal

---
+++ src/__tests__/usePromisePortal.unit.test.ts
@@ -0,0 +1,12 @@
+import { renderHook } from "@testing-library/react-hooks";
+import { PromisePortalActions } from '../types';
+import usePromisePortal from '../usePromisePortal';
+
+describe('usePromisePortal', () => {
+  const { result: { current } } = renderHook(() => usePromisePortal());
+  const context = current as PromisePortalActions;
+
+  it('exposes showPortalAsync function', () => {
+    expect(context.showPortalAsync).toBeDefined();    
+  });
+});
