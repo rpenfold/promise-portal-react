8877f3da04f1b3c38578d030f5f1620e0558ae19
Ryan Penfold <rpenfold@live.com>
2021-05-28 21:38:09 -0600

    updating unit tests after refactor

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,6 @@ module.exports = {
   setupFiles: [
     "./config/testSetup.js"
   ],
-  
   collectCoverage: true,
   collectCoverageFrom: [
     "src/**/*",
--- src/__tests__/PromiseComponent.unit.test.tsx
+++ src/__tests__/PromiseComponent.unit.test.tsx
@@ -1,91 +1,78 @@
-import React from "react";
+import React, { ComponentType } from "react";
 import { shallow, ShallowWrapper } from "enzyme";
 import PromiseComponent,
-    { Props as InternalPromiseComponentProps }
-from "../PromiseComponent";
+{ Props as InternalPromiseComponentProps }
+  from "../PromiseComponent";
+import { PromiseComponentProps, Portal } from "../types";
 
-class MockComponent extends React.Component {}
+class MockComponent extends React.Component { }
 type Wrapper = ShallowWrapper<InternalPromiseComponentProps, null, PromiseComponent>
+interface PropsFactoryInput { index?: number; data?: Partial<Portal> }
 
 const getProps = ({
-    index = 0,
-    data = {}
-} = {}): InternalPromiseComponentProps => ({
-    componentKey: "some_component_key",
-    data: {
-        Component: MockComponent,
-        promiseInfo: {
-            reject: jest.fn(),
-            resolve: jest.fn(),
-        },
-        ...data
-    },
-    index,
+  index = 0,
+  data = {}
+}: PropsFactoryInput = {}): InternalPromiseComponentProps => ({
+  data: {
+    Component: MockComponent as ComponentType<PromiseComponentProps>,
     onCancel: jest.fn(),
     onComplete: jest.fn(),
     onError: jest.fn(),
+    onRequestClose: jest.fn(),
+    id: 123,
+    open: true,
+    forceShow: false,
+    ...data
+  },
+  index,
 });
 
 describe("<PromiseComponent/>", () => {
-    let props: InternalPromiseComponentProps;
-    let wrapper: Wrapper;
+  let props: InternalPromiseComponentProps;
+  let wrapper: Wrapper;
 
-    beforeEach(() => {
-        props = getProps();
-        wrapper = shallow(<PromiseComponent {...props}/>);
+  beforeEach(() => {
+    props = getProps();
+    wrapper = shallow(<PromiseComponent {...props} />);
+  });
+
+  describe("render", () => {
+    it("does not render null", () => {
+      expect(wrapper.isEmptyRender()).toBe(false);
     });
 
-    describe("render", () => {
-        it("does not render null", () => {
-            expect(wrapper.isEmptyRender()).toBe(false);
-        });
-
-        it("renders nothing if not at bottom of stack", () => {
-            wrapper.setProps({ index: 1 });
-            expect(wrapper.isEmptyRender()).toBe(true);
-        });
-
-        it("renders nothing if there was an error", () => {
-            wrapper.setState({ hasErrors: true });
-            expect(wrapper.isEmptyRender()).toBe(true);
-        });
-
-        it("renders if not at the bottom of the stack but is force shown", () => {
-            props = getProps({ index: 1, data: { forceShow: true } })
-            wrapper.setProps(props);
-            expect(wrapper.isEmptyRender()).toBe(false);
-        });
+    it("renders nothing if not at bottom of stack", () => {
+      wrapper.setProps({ index: 1 });
+      expect(wrapper.isEmptyRender()).toBe(true);
     });
 
-    describe("instance.componentDidCatch", () => {
-        it("invokes onError callback", () => {
-            const error = new Error("mock_error");
-            wrapper.simulateError(error);
-            expect(props.onError).toBeCalledWith(props.componentKey, error, expect.anything())
-        });
-
-        it("sets state.hasErrors to be true", () => {
-            wrapper.simulateError(new Error());
-            expect(wrapper.state('hasErrors')).toBe(true);
-        });
-
-        it("does not render after error is caught", () => {
-            wrapper.simulateError(new Error());
-            expect(wrapper.isEmptyRender()).toBe(true);
-        })
+    it("renders nothing if there was an error", () => {
+      wrapper.setState({ hasErrors: true });
+      expect(wrapper.isEmptyRender()).toBe(true);
     });
 
-    describe("instance.handleCancel", () => {
-        it("invokes onCancel callback", () => {
-            wrapper.instance().handleCancel();
-            expect(props.onCancel).toBeCalledWith(props.componentKey);
-        });
+    it("renders if not at the bottom of the stack but is force shown", () => {
+      props = getProps({ index: 1, data: { forceShow: true } })
+      wrapper.setProps(props);
+      expect(wrapper.isEmptyRender()).toBe(false);
+    });
+  });
+
+  describe("instance.componentDidCatch", () => {
+    it("invokes onError callback", () => {
+      const error = new Error("mock_error");
+      wrapper.simulateError(error);
+      expect(props.data.onError).toBeCalledWith(error, expect.anything())
     });
 
-    describe("instance.handleComplete", () => {
-        it("invokes onComplete callback", () => {
-            wrapper.instance().handleComplete();
-            expect(props.onComplete).toBeCalledWith(props.componentKey, props.data);
-        });
+    it("sets state.hasErrors to be true", () => {
+      wrapper.simulateError(new Error());
+      expect(wrapper.state('hasErrors')).toBe(true);
     });
+
+    it("does not render after error is caught", () => {
+      wrapper.simulateError(new Error());
+      expect(wrapper.isEmptyRender()).toBe(true);
+    })
+  });
 });
