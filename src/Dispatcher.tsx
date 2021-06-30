33f1553c43b19e0d12a33cc718fb0a52f0188771
Ryan Penfold <rpenfold@live.com>
2021-06-29 21:24:27 -0600

    fix for dispatcher

--- src/Dispatcher.tsx
+++ src/Dispatcher.tsx
@@ -1,4 +1,4 @@
-import React, { PureComponent, ReactNode } from "react";
+import React, { Component, ReactNode } from "react";
 import {
   ComponentParam,
   ComponentProps,
@@ -10,8 +10,13 @@ import {
  * Dispatcher is a utility that exposes promise-portal functionality to outside
  * React components.
  */
-class Dispatcher extends PureComponent<PromisePortalActions> {
+class Dispatcher extends Component<PromisePortalActions> {
   private static instance: Dispatcher;
+
+  constructor(props: PromisePortalActions) {
+    super(props);
+    Dispatcher.instance = this;
+  }
 
   static showPortalAsync = <T,>(
     component: ComponentParam,
@@ -25,7 +30,9 @@ class Dispatcher extends PureComponent<PromisePortalActions> {
   };
 
   render(): ReactNode {
-    Dispatcher.instance = this;
+    if (!Dispatcher.instance) {
+      Dispatcher.instance = this;
+    }
 
     return <React.Fragment />;
   }
