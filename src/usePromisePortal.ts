87a40f2fc3efbf4e3c5df28365e273c7363bc32a
Ryan Penfold <rpenfold@live.com>
2021-07-20 21:02:11 -0600

    fixed usability issue with usePromisePortal

--- src/usePromisePortal.ts
+++ src/usePromisePortal.ts
@@ -2,6 +2,6 @@ import React from "react";
 import PromisePortalContext from "./PromisePortalContext";
 import { PromisePortalActions } from "./types";
 
-export default function usePromisePortal(): PromisePortalActions | null {
+export default function usePromisePortal(): PromisePortalActions {
   return React.useContext<PromisePortalActions>(PromisePortalContext);
 }
