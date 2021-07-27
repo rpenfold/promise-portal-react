a4f2d0617babdf2ddfe6cfef80ea80071c260875
rpenfold <rpenfold@live.com>
2021-07-27 16:52:17 -0600

    Update types.ts

--- src/PromisePortalProvider/types.ts
+++ src/PromisePortalProvider/types.ts
@@ -2,6 +2,8 @@ import { Portal } from "types";
 
 export type SetPortals = React.Dispatch<React.SetStateAction<Portal[]>>;
 
+export type MatchPortalPredicate = (componentName: string, props: ComponentProps) => boolean;
+
 export interface ProviderInternalContext {
   removePortal(id: string): void;
   requestClosePortal(id: string): void;
